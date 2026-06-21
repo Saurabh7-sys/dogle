2ddfe06e3ae2457d16c013a687a615f7a4d0a9cf
Saurabh Shelar <saurabhshelar512@gmail.com>
2026-06-21 14:21:09 +0530

    exclude admin routes from sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://bmpetcare.in',
+  siteUrl: "https://bmpetcare.in",
   generateRobotsTxt: true,
-}
+
+  exclude: [
+    "/admin",
+    "/admin/*",
+    "/admin/login",
+    "/admin/users",
+    "/admin/settings",
+    "/admin/bookings",
+    "/admin/enquiries",
+    "/admin/pages",
+  ],
+};
